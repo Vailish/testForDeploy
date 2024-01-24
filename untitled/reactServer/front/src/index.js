7528b77e5b0b777646a5780f34a77e42f78e8322
vailish <joyksj94@gmail.com>
2024-01-24 10:22:01 +0900

    update front

---
+++ untitled/reactServer/front/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import {BrowserRouter} from 'react-router-dom';
+import Footer from './layout/Footer';
+import Header from './layout/Header';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <Header />
+    <App />
+    <Footer />
+  </BrowserRouter>
+);
---
+++ untitled/reactServer/front/src/routes/BoardList.js
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+const BoardList = () => {
+    const [boardList, setBoardList] = useState([]);
+    
+    const getBoardList = async () => {
+        const resp = (await axios.get('//localhost:8080/api/v1/data')).data
+        setBoardList(resp);
+        console.log(BoardList);
+
+        const pngn = resp.pagination;
+        console.log(pngn);
+    }
+
+    useEffect(() => {
+        getBoardList();
+    }, []);
+
+    return (
+        <div>
+            <ul>
+                {boardList.map((board) => (
+                    <li key={board.testId}>{board.cnt}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default BoardList;
